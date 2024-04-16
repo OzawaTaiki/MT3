ba765a8eeb14c6d0c154be28e0e41691c6db65b1
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-04-17 00:12:33 +0900

    MT3_00_02

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "VectorFunction.h"
 #include "MatrixFunction.h"
 
-const char kWindowTitle[] = "LE2A_07_オザワ_タイキ_MT3_00_00";
+const char kWindowTitle[] = "LE2A_07_オザワ_タイキ_MT3_00_02";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -14,7 +14,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-
+	Matrix4x4 m1 = {
+		3.2f,0.7f,9.6f,4.4f,
+		5.5f,1.3f,7.8f,2.1f,
+		6.9f,8.0f,2.6f,1.0f,
+		0.5f,7.2f,5.1f,3.3f
+	};
+	Matrix4x4 m2 = {
+		4.1f,6.5f,3.3f,2.2f,
+		8.8f,0.6f,9.9f,7.7f,
+		1.1f,5.5f,6.6f,0.0f,
+		3.3f,9.9f,8.8f,2.2f
+	};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -29,7 +40,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-
+		Matrix4x4 resultAdd = MatrixFunction::Add(m1, m2);
+		Matrix4x4 resultMultiply = MatrixFunction::Multiply(m1, m2);
+		Matrix4x4 resultSubtract = MatrixFunction::Subtract(m1, m2);
+		Matrix4x4 inverseM1 = MatrixFunction::Inverse(m1);
+		Matrix4x4 inverseM2 = MatrixFunction::Inverse(m2);
+		Matrix4x4 transposeM1 = MatrixFunction::Transpose(m1);
+		Matrix4x4 transposeM2 = MatrixFunction::Transpose(m2);
+		Matrix4x4 identity = MatrixFunction::MakeIdentity4x4();
 
 		///
 		/// ↑更新処理ここまで
@@ -39,7 +57,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-
+		MatrixFunction::MatrixScreenPrintf(0, 0, resultAdd, "Add");
+		MatrixFunction::MatrixScreenPrintf(0, kRowHeight * 5, resultSubtract, "Subtract");
+		MatrixFunction::MatrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultiply, "Multiply");
+		MatrixFunction::MatrixScreenPrintf(0, kRowHeight * 5 * 3, inverseM1, "inverseM1");
+		MatrixFunction::MatrixScreenPrintf(0, kRowHeight * 5 * 4, inverseM2, "inverseM2");
+		MatrixFunction::MatrixScreenPrintf(kColumnWidth * 5, 0, transposeM1, "transposeM1");
+		MatrixFunction::MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5, transposeM2, "transposeM2");
+		MatrixFunction::MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5 * 2, identity, "identity");
 
 		///
 		/// ↑描画処理ここまで
