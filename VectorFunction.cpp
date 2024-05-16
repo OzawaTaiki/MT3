d84a29ab5ddd980a5288e578fcc2f18b2d945351
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-05-16 22:07:08 +0900

    VectorFunctionを修正

--- VectorFunction.cpp
+++ VectorFunction.cpp
@@ -1,53 +1,53 @@
 #include "VectorFunction.h"
-#include<cmath>
-#include<Novice.h>
+#include <cmath>
+#include <Novice.h>
 #include <assert.h>
 
 static const int kColumnWidth = 60;
 
-Vector3 VectorFunction::Add(const Vector3& v1, const Vector3& v2)
+Vector3 VectorFunction::Add(const Vector3& _v1, const Vector3& _v2)
 {
 	Vector3 result;
-	result.x = v1.x + v2.x;
-	result.y = v1.y + v2.y;
-	result.z = v1.z + v2.z;
+	result.x = _v1.x + _v2.x;
+	result.y = _v1.y + _v2.y;
+	result.z = _v1.z + _v2.z;
 
 	return result;
 }
 
-Vector3 VectorFunction::Subtract(const Vector3& v1, const Vector3& v2)
+Vector3 VectorFunction::Subtract(const Vector3& _v1, const Vector3& _v2)
 {
 	Vector3 result;
-	result.x = v1.x - v2.x;
-	result.y = v1.y - v2.y;
-	result.z = v1.z - v2.z;
+	result.x = _v1.x - _v2.x;
+	result.y = _v1.y - _v2.y;
+	result.z = _v1.z - _v2.z;
 
 	return result;
 }
 
-Vector3 VectorFunction::Multiply(float scalar, const Vector3& v)
+Vector3 VectorFunction::Multiply(float _scalar, const Vector3& _v)
 {
 	Vector3 result;
-	result.x = v.x * scalar;
-	result.y = v.y * scalar;
-	result.z = v.z * scalar;
+	result.x = _v.x * _scalar;
+	result.y = _v.y * _scalar;
+	result.z = _v.z * _scalar;
 
 	return result;
 }
 
-Vector3 VectorFunction::Multiply(const Vector3& v1, const Vector3& v2)
+Vector3 VectorFunction::Multiply(const Vector3& _v1, const Vector3& _v2)
 {
 	Vector3 result;
-	result.x = v1.x * v2.x;
-	result.y = v1.y * v2.y;
-	result.z = v1.z * v2.z;
+	result.x = _v1.x * _v2.x;
+	result.y = _v1.y * _v2.y;
+	result.z = _v1.z * _v2.z;
 
 	return result;
 }
 
-float VectorFunction::Dot(const Vector3& v1, const Vector3& v2)
+float VectorFunction::Dot(const Vector3& _v1, const Vector3& _v2)
 {
-	float result = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	float result = _v1.x * _v2.x + _v1.y * _v2.y + _v1.z * _v2.z;
 
 	return result;
 }
@@ -63,53 +63,53 @@ Vector3 VectorFunction::Cross(const Vector3& _v1, const Vector3& _v2)
 	return result;
 }
 
-float VectorFunction::length(const Vector3& v)
+float VectorFunction::length(const Vector3& _v)
 {
 	float result;
-	result = std::sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	result = std::sqrtf(_v.x * _v.x + _v.y * _v.y + _v.z * _v.z);
 	return result;
 }
 
-Vector3 VectorFunction::Normalize(const Vector3& v)
+Vector3 VectorFunction::Normalize(const Vector3& _v)
 {
 	Vector3 result;
-	float lenght = length(v);
-	if (lenght == 0)
+	float _length = length(_v);
+	if (_length == 0)
 		return { 0 };
-	result.x = v.x / lenght;
-	result.y = v.y / lenght;
-	result.z = v.z / lenght;
+	result.x = _v.x / _length;
+	result.y = _v.y / _length;
+	result.z = _v.z / _length;
 
 	return result;
 }
 
-void VectorFunction::VectorScreenPrintf(int x, int y, const Vector3& v)
+void VectorFunction::VectorScreenPrintf(int _x, int _y, const Vector3& _v)
 {
-	Novice::ScreenPrintf(x, y, "%.02f", v.x);
-	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", v.y);
-	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", v.z);
+	Novice::ScreenPrintf(_x, _y, "%.02f", _v.x);
+	Novice::ScreenPrintf(_x + kColumnWidth, _y, "%.02f", _v.y);
+	Novice::ScreenPrintf(_x + kColumnWidth * 2, _y, "%.02f", _v.z);
 }
 
-void VectorFunction::VectorScreenPrintf(int x, int y, const Vector3& v, const char* label)
+void VectorFunction::VectorScreenPrintf(int _x, int _y, const Vector3& _v, const char* _label)
 {
-	Novice::ScreenPrintf(x, y, "%.02f", v.x);
-	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", v.y);
-	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", v.z);
-	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+	Novice::ScreenPrintf(_x, _y, "%.02f", _v.x);
+	Novice::ScreenPrintf(_x + kColumnWidth, _y, "%.02f", _v.y);
+	Novice::ScreenPrintf(_x + kColumnWidth * 2, _y, "%.02f", _v.z);
+	Novice::ScreenPrintf(_x + kColumnWidth * 3, _y, "%s", _label);
 }
 
-Vector3 VectorFunction::Transform(const Vector3& vector, const Matrix4x4& matrix)
+Vector3 VectorFunction::Transform(const Vector3& _vector, const Matrix4x4& _matrix)
 {
 	Vector3 result;
 
-	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
-	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
-	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
-	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
-	assert(w != 0.0f);
-	result.x /= w;
-	result.y /= w;
-	result.z /= w;
+	result.x = _vector.x * _matrix.m[0][0] + _vector.y * _matrix.m[1][0] + _vector.z * _matrix.m[2][0] + 1.0f * _matrix.m[3][0];
+	result.y = _vector.x * _matrix.m[0][1] + _vector.y * _matrix.m[1][1] + _vector.z * _matrix.m[2][1] + 1.0f * _matrix.m[3][1];
+	result.z = _vector.x * _matrix.m[0][2] + _vector.y * _matrix.m[1][2] + _vector.z * _matrix.m[2][2] + 1.0f * _matrix.m[3][2];
+	float _w = _vector.x * _matrix.m[0][3] + _vector.y * _matrix.m[1][3] + _vector.z * _matrix.m[2][3] + 1.0f * _matrix.m[3][3];
+	assert(_w != 0.0f);
+	result.x /= _w;
+	result.y /= _w;
+	result.z /= _w;
 	return result;
 }
 
@@ -135,6 +135,5 @@ Vector3 operator/(const Vector3& _v1, const Vector3& _v2)
 
 Vector3 operator*(const Vector3& _v, float _s)
 {
-	return 	Vector3(_v.x * _s, _v.y * _s, _v.z * _s);
-
+	return Vector3(_v.x * _s, _v.y * _s, _v.z * _s);
 }
--- VectorFunction.h
+++ VectorFunction.h
@@ -7,17 +7,17 @@
 class VectorFunction
 {
 public:
-	static Vector3 Add(const Vector3& v1, const Vector3& v2);
-	static Vector3 Subtract(const Vector3& v1, const Vector3& v2);
-	static Vector3 Multiply(float scalar, const Vector3& v);
-	static Vector3 Multiply(const Vector3& v1, const Vector3& v2);
-	static float Dot(const Vector3& v1, const Vector3& v2);
+	static Vector3 Add(const Vector3& _v1, const Vector3& _v2);
+	static Vector3 Subtract(const Vector3& _v1, const Vector3& _v2);
+	static Vector3 Multiply(float _scalar, const Vector3& _v);
+	static Vector3 Multiply(const Vector3& _v1, const Vector3& _v2);
+	static float Dot(const Vector3& _v1, const Vector3& _v2);
 	static Vector3 Cross(const Vector3& _v1, const Vector3& _v2);
-	static float length(const Vector3& v);
-	static Vector3 Normalize(const Vector3& v);
-	static void VectorScreenPrintf(int x, int y, const Vector3& v);
-	static void VectorScreenPrintf(int x, int y, const Vector3& v, const char* label);
-	static Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+	static float length(const Vector3& _v);
+	static Vector3 Normalize(const Vector3& _v);
+	static void VectorScreenPrintf(int _x, int _y, const Vector3& _v);
+	static void VectorScreenPrintf(int _x, int _y, const Vector3& _v, const char* _label);
+	static Vector3 Transform(const Vector3& _vector, const Matrix4x4& _matrix);
 };
 
 Vector3 operator+(const Vector3& _v1, const Vector3& _v2);
