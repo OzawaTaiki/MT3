e26f77e15cecffae6f21b99aaf7497ad471214b9
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-06-10 10:28:11 +0900

    タイトルバーの変更

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 #include <imgui.h>
 
-const char kWindowTitle[] = "LE2A_07_オザワ_タイキ_MT3_02_08";
+const char kWindowTitle[] = "LE2A_07_オザワ_タイキ_MT3";
 
 static const int kWindowWidth = 1280;
 static const int kWindowHeight = 720;
@@ -44,43 +44,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		/*ImGui::Begin("OBB1");
-		ImGui::DragFloat3("center", &obb1.center.x, 0.01f);
-		ImGui::DragFloat3("orientations[0]", &obb1.orientations[0].x, 0.01f);
-		ImGui::DragFloat3("orientations[1]", &obb1.orientations[1].x, 0.01f);
-		ImGui::DragFloat3("orientations[2]", &obb1.orientations[2].x, 0.01f);
-		ImGui::DragFloat3("size", &obb1.size.x, 0.01f);
-		ImGui::DragFloat3("rotate", &obb1.rotate.x, 0.01f);
-		ImGui::End();
-		obb1.CalculateOrientations();*/
-
-		/*ImGui::Begin("sphere");
-		ImGui::DragFloat3("center", &sphere.center.x, 0.01f);
-		ImGui::DragFloat("radius", &sphere.radius, 0.01f);
-		ImGui::End();*/
-
-		/*ImGui::Begin("AABB");
-		ImGui::DragFloat3("Min", &aabb.min.x, 0.01f);
-		ImGui::DragFloat3("Max", &aabb.max.x, 0.01f);
-		ImGui::End();
-		aabb.Update();*/
-
-		/*ImGui::Begin("segment");
-		ImGui::DragFloat3("origin", &segment.origin.x, 0.01f);
-		ImGui::DragFloat3("diff", &segment.diff.x, 0.01f);
-		ImGui::End();*/
-
-		/*ImGui::Begin("Plane");
-		ImGui::DragFloat3("normal", &plane.normal.x, 0.01f);
-		ImGui::DragFloat("Distance", &plane.distance, 0.01f);
-		ImGui::End();
-		plane.normal = VectorFunction::Normalize(plane.normal);*/
-
-		/*ImGui::Begin("Bezier");
-		ImGui::DragFloat3("point_0", &bezier.point[0].x, 0.01f);
-		ImGui::DragFloat3("point_1", &bezier.point[1].x, 0.01f);
-		ImGui::DragFloat3("point_2", &bezier.point[2].x, 0.01f);
-		ImGui::End();*/
 
 		ImGui::Begin("CatmullRom");
 		ImGui::DragFloat3("point_0", &cpoint[0].x, 0.01f);
@@ -89,7 +52,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		ImGui::DragFloat3("point_3", &cpoint[3].x, 0.01f);
 		ImGui::End();
 
-		//bool isCollisin = IsCollision(obb1, obb2);
 
 		///
 		/// ↑更新処理ここまで
@@ -102,11 +64,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		DrawGrid(camera->GetviewProjectionMatrix(), camera->GetViewportMatrix());
 
 		DrawCatmullRom(cpoint[0], cpoint[1], cpoint[2], cpoint[3], camera->GetviewProjectionMatrix(), camera->GetViewportMatrix(), BLUE, 1);
-		//DrawBezier(bezier, camera->GetviewProjectionMatrix(), camera->GetViewportMatrix(), BLUE, 1);
-		//DrawOBB(obb1, camera->GetviewProjectionMatrix(), camera->GetViewportMatrix(), isCollisin ? RED : WHITE);
-		//Drawline(segment.origin, segment.diff, camera->GetviewProjectionMatrix(), camera->GetViewportMatrix(), WHITE);
-		//DrawSphere(sphere, camera->GetviewProjectionMatrix(), camera->GetViewportMatrix(), WHITE);
-		//DrawAABB(aabb, camera->GetviewProjectionMatrix(), camera->GetViewportMatrix(), isCollisin ? RED : WHITE);
 
 		///
 		/// ↑描画処理ここまで
